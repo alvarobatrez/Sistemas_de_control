ae0feec0049992094105a4f81968eba39b53c791
alvarobatrez <alvarobatrez27@gmail.com>
2022-06-01 21:58:34 +0200

    se agrego p2_bode

--- control_continuo/p6_bode/p2_bode.m
+++ control_continuo/p6_bode/p2_bode.m
@@ -1,15 +1,8 @@
 close all; clear, clc
 
-% G = (1.151*s + 0.1774)/(s^3 + 0.739*s^2 + 0.921*s)
-
-w = 0.1:0.1:10;
+w = [0.1 0.5 1 5 10];
 s = w.*1i;
-G = (1.151*s + 0.1774)./(s.^3 + 0.739*s.^2 + 0.921*s);
-A = abs(G);
-dB = 20*log10(A);
-phi = angle(G)*180/pi;
-
-subplot(2,1,1)
-semilogx(w,dB), grid on, xlabel('omega'), ylabel('dB')
-subplot(2,1,2)
-semilogx(w,phi), grid on, xlabel('omega'), ylabel('Deg')
+G = 1 ./ (s.^2 + 0.5*s + 1)
+A = sqrt(real(G).^2 + imag(G).^2)
+db = 20*log10(A)
+phi = atan2d(imag(G),real(G))
